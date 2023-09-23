1e0cdbcae07a0d954c73f990fb50c41af17b52af
acer <acer@192.168.1.37>
2023-09-23 14:43:17 +0530

    second commit

---
+++ project4/src/com/tns/project4/Encapsulation.java
@@ -0,0 +1,14 @@
+package com.tns.project4;
+
+public class Encapsulation {
+	//data members
+	public int serialno;
+	public String name;
+	public int age;
+	
+	//member function
+
+	public void show() {
+		System.out.println("serialno"+ serialno+ "\t"+"name"+name + "\t"+ "age" +age);
+	}
+}
---
+++ project4/src/com/tns/project4/Encapsulationdemo.java
@@ -0,0 +1,15 @@
+package com.tns.project4;
+
+public class Encapsulationdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Encapsulation ob=new Encapsulation();//created object
+       ob.serialno=33;
+       ob.name="hari";
+       ob.age=22;
+       ob.show();
+       
+	}
+
+}

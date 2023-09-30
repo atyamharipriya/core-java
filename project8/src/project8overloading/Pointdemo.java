e0d580efa0383f74bca1c22fedac44ab93a45267
acer <acer@10.0.0.105>
2023-09-30 14:44:49 +0530

    First commit

---
+++ project8/src/project8overloading/Pointdemo.java
@@ -0,0 +1,11 @@
+package project8overloading;
+
+public class Pointdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Point p=new Point();
+      System.out.println(p);
+	}
+
+}

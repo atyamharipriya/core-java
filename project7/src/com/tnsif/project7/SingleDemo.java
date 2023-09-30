df930ad9792dbb532080efc3c807f48a0d546cfc
acer <acer@10.0.0.105>
2023-09-30 14:43:16 +0530

    First commit

---
+++ project7/src/com/tnsif/project7/SingleDemo.java
@@ -0,0 +1,11 @@
+package com.tnsif.project7;
+
+public class SingleDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         Student st=new Student("hari",1233456,"penugonda",154673);
+         System.out.println(st);
+         }
+
+}
---
+++ project7/src/com/tnsif/project7/Student.java
@@ -0,0 +1,37 @@
+package com.tnsif.project7;
+
+public class Student extends Citizen {
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	private int rollno;
+	private String collegename;
+	public Student(int rollno, String collegename) {
+		super();
+		this.rollno = rollno;
+		this.collegename = collegename;
+	}
+	public Student(String name,long aadharno,String address,long phno) {
+		 super(name,aadharno,address,phno);
+	}
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	public String getCollegename() {
+		return collegename;
+	}
+	public void setCollegename(String collegename) {
+		this.collegename = collegename;
+	}
+	@Override
+	public String toString() {
+		return "Student [rollno=" + rollno + ", collegename=" + collegename + "]";
+	}
+	//}
+	
+	}

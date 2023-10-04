0cdff779e7288b38d87ba11dd69324b9c003b2d2
acer <acer@10.0.0.123>
2023-10-04 14:16:15 +0530

    first commit

---
+++ project9/src/com/tnsif/Interface/InExec.java
@@ -0,0 +1,13 @@
+package com.tnsif.Interface;
+
+public class InExec {
+	
+	public static void main(String[] args) {
+		InDemo i=new InDemo();
+		i.show();
+		i.print();
+	}
+	
+	}
+
+
---
+++ project9/src/com/tnsif/abstraction/Demo.java
@@ -0,0 +1,22 @@
+package com.tnsif.abstraction;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Square s=new Square();
+Rectangle r=new Rectangle();
+System.out.println(s);
+System.out.println(r);
+Shape sh;
+sh=new Square(4.0f);
+sh.calArea();
+sh.show();
+
+sh=new Rectangle(4.0f,5.5f);
+sh.calArea();
+sh.show();
+
+	}
+
+}

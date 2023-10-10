f4e836e56a88e2831e7fbc2fc454499d8eb2231c
acer <acer@10.0.0.120>
2023-10-10 16:28:16 +0530

    sixth commit

---
+++ project9/src/com/tnsif/Staticmethod/DemoClass.java
@@ -0,0 +1,14 @@
+package com.tnsif.Staticmethod;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MtClass.display();
+		MtClass o1=new MtClass();
+		MtClass.display();
+		System.out.println(o1);
+
+	}
+
+}
---
+++ project9/src/com/tnsif/Staticmethod/MtClass.java
@@ -0,0 +1,27 @@
+package com.tnsif.Staticmethod;
+
+public class MtClass {
+	private int section ;
+		private static int srNo;
+		static {
+			System.out.println("static block");
+			srNo=1000;
+			}
+		
+	MtClass(){
+		System.out.println("default constructor");
+		srNo++;
+		section++;
+	}
+
+	@Override
+	public String toString() {
+		return "MtClass [section=" + section + "]";
+	}
+	   static void display() {
+		   System.out.println("serialno"+srNo);
+	   }
+	}
+	
+
+

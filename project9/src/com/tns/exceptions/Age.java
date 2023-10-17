b87db98ee8ed6ad92b6fd80fb123cffbe777388b
acer <acer@192.168.1.43>
2023-10-17 14:32:21 +0530

    commit

---
+++ project9/src/com/tns/exceptions/Age.java
@@ -0,0 +1,8 @@
+package com.tns.exceptions;
+
+public class Age extends Exception {
+	Age(String str){
+		System.out.println(str);
+	}
+
+}
---
+++ project9/src/com/tns/exceptions/UserDefined.java
@@ -0,0 +1,18 @@
+package com.tns.exceptions;
+
+import java.util.Scanner;
+
+public class UserDefined {
+	private static int age;
+	
+	static void validate() throws Age {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter age");
+		age=sc.nextInt();
+		if(age<18)
+			throw new Age ("invalid age,you are not eligible");
+		else
+			System.out.println("you can vote");
+	}
+
+}

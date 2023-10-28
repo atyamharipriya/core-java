dd58b9b0880dcbdd364c2eb943f6fb7e47d989f7
acer <acer@192.168.1.47>
2023-10-28 15:15:25 +0530

    eigth commit

---
+++ project9/src/com/tns/collection/ArrayListDemo.java
@@ -0,0 +1,27 @@
+package com.tns.collection;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    		ArrayList list1=new ArrayList();
+    		System.out.println("check the list"+list1.isEmpty());
+    		list1.add(1);
+    		list1.add('w');
+    		list1.add(3);
+    		System.out.println("the size of the list"+list1.size());
+    		System.out.println("the list elements are"+list1);
+    		System.out.println("the elements is in list"+list1.contains(55));
+    		System.out.println("remove the element in the list"+list1.remove(1));
+    		System.out.println("element at the position in the list"+list1.get(0));
+    		Iterator<Integer>i=list1.iterator();
+    		while(i.hasNext()) {
+    			Integer p=i.next();
+    			System.out.println(p);
+    		}
+    	}
+    	
+}
